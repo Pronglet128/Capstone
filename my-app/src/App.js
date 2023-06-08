bfc63cca356ec81eb23a2c961766f65996a83b9f
Pronglet128 <hedviggirl@Gmail.com>
2023-06-08 23:19:27 +0200

    first html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      homepage
-      pepa.run
-    </div>
+    <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
