2d62655e1528f76380c2e294a8fa70d194dfb78e
Pronglet128 <hedviggirl@Gmail.com>
2023-06-16 22:45:32 +0200

    Style update 2

--- src/Main.js
+++ src/Main.js
@@ -6,25 +6,28 @@ const Main = () => {
         <h2 className="title">Specials</h2>
         <button className="button">Online Menu</button>
         <div className="card card_left">
-            <img src="greek salad.jpg" alt="greek salad" />
+            <img className="food_photo" src="greek salad.jpg" alt="greek salad" />
             <h3>Greek salad</h3>
-            <p>$12.99</p>
+            <p className="price">$12.99</p>
             <p className="text">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-            <button>Order a delivery</button>
+            <a href="/order">Order a delivery</a>
+            <img className="bike" src="bicycle.png" alt="delivery"/>
         </div>
         <div className="card card_center">
-            <img src="Screenshot_1.png" alt="bruchetta" />
+            <img className="food_photo" src="Screenshot_1.png" alt="bruchetta" />
             <h3>Bruchetta</h3>
-            <p>$ 5.99</p>
+            <p className="price">$ 5.99</p>
             <p className="text">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
-            <button>Order a delivery</button>
+            <a href="/order">Order a delivery</a>
+            <img className="bike" src="bicycle.png" alt="delivery"/>
         </div>
         <div className="card card_right">
-            <img src="lemon dessert.jpg" alt="lemon dessert" />
+            <img className="food_photo" src="lemon dessert.jpg" alt="lemon dessert" />
             <h3>Lemon Dessert</h3>
-            <p>$ 5.00</p>
+            <p className="price">$ 5.00</p>
             <p className="text">This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
-            <button>Order a delivery</button>
+            <a href="/order">Order a delivery</a>
+            <img className="bike" src="bicycle.png" alt="delivery"/>
         </div>
     </main>
     )
